61750dba58e40a3089955a197ee1c9fd0b36a118
raulhdz_dev <raul.hernandez.lopez.dev@gmail.com>
2025-08-11 12:19:38 -0600

    Se recibe el nombre del amigo y se valida que no esté vacío

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let amigo = "";
+
+function agregarAmigo() {
+    amigo = document.getElementById('amigo').value;
+    console.log(amigo);
+
+    if (amigo != "") {
+        listaAmigos.push(amigo)
+    } else {
+        alert("Por favor ingresa un nombre valido");
+    }
+    return;
+}
